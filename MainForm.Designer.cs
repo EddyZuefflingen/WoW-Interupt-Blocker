92beb2a279f53f3552b818d00a80380fdf39e428
Etienne Kok <ekok@rwdata.de>
2023-04-23 17:40:17 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace InteruptBlocker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pMain = new MetroFramework.Controls.MetroPanel();
+            this.lblInteruptCount = new MetroFramework.Controls.MetroLabel();
+            this.lblInteruptCountVal = new MetroFramework.Controls.MetroLabel();
+            this.pMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pMain
+            // 
+            this.pMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pMain.Controls.Add(this.lblInteruptCountVal);
+            this.pMain.Controls.Add(this.lblInteruptCount);
+            this.pMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pMain.HorizontalScrollbarBarColor = true;
+            this.pMain.HorizontalScrollbarHighlightOnWheel = false;
+            this.pMain.HorizontalScrollbarSize = 10;
+            this.pMain.Location = new System.Drawing.Point(20, 60);
+            this.pMain.Name = "pMain";
+            this.pMain.Size = new System.Drawing.Size(212, 46);
+            this.pMain.TabIndex = 0;
+            this.pMain.VerticalScrollbarBarColor = true;
+            this.pMain.VerticalScrollbarHighlightOnWheel = false;
+            this.pMain.VerticalScrollbarSize = 10;
+            // 
+            // lblInteruptCount
+            // 
+            this.lblInteruptCount.AutoSize = true;
+            this.lblInteruptCount.Location = new System.Drawing.Point(11, 14);
+            this.lblInteruptCount.Name = "lblInteruptCount";
+            this.lblInteruptCount.Size = new System.Drawing.Size(87, 19);
+            this.lblInteruptCount.TabIndex = 2;
+            this.lblInteruptCount.Text = "Keys blocked:";
+            // 
+            // lblInteruptCountVal
+            // 
+            this.lblInteruptCountVal.AutoSize = true;
+            this.lblInteruptCountVal.Location = new System.Drawing.Point(98, 14);
+            this.lblInteruptCountVal.Name = "lblInteruptCountVal";
+            this.lblInteruptCountVal.Size = new System.Drawing.Size(16, 19);
+            this.lblInteruptCountVal.TabIndex = 3;
+            this.lblInteruptCountVal.Text = "0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 126);
+            this.Controls.Add(this.pMain);
+            this.Name = "MainForm";
+            this.Resizable = false;
+            this.Text = "Interupt Blocker";
+            this.pMain.ResumeLayout(false);
+            this.pMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroPanel pMain;
+        private MetroFramework.Controls.MetroLabel lblInteruptCount;
+        private MetroFramework.Controls.MetroLabel lblInteruptCountVal;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,91 @@
+﻿using Gma.System.MouseKeyHook;
+using MetroFramework;
+using MetroFramework.Forms;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InteruptBlocker
+{
+    public partial class MainForm : MetroForm
+    {
+        Point castbarLocation = new Point(1534, 1061); //Point on Screen where the cast bar of the enemy is
+        Color interuptableCastbarColor = Color.FromArgb(255, 186, 41); //Color of the interupable cast
+
+        public MainForm()
+        {
+            InitializeComponent();
+            StartKeyListener();
+        }
+
+        IKeyboardMouseEvents m_GlobalHook;
+        int InteruptBlockedCount = 0;
+
+        public void StartKeyListener()
+        {
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.KeyDown += GlobalHookKeyDown;
+        }
+
+        private void GlobalHookKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Control.ModifierKeys != Keys.Shift && e.KeyCode == Keys.F)
+            {
+                var color = GetColorAt(castbarLocation);
+                if (GetActiveWindowTitle() == "World of Warcraft")
+                {
+                    if (color.R == interuptableCastbarColor.R && color.G == interuptableCastbarColor.R && color.B == interuptableCastbarColor.B)
+                    {
+                        lblInteruptCountVal.Text = "" + InteruptBlockedCount;
+                        InteruptBlockedCount++;
+                        Console.WriteLine($"Keys Supressed: {InteruptBlockedCount}");
+                        e.SuppressKeyPress = true;
+                    }
+                }
+            }
+        }
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        public Color GetColorAt(Point location)
+        {
+            Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+    }
+}
